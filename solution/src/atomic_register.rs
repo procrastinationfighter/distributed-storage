2cb30c9a7656b5056f4e523ed7abfb0419c7a009
Adam Boguszewski <ab417730@students.mimuw.edu.pl>
2023-12-24 14:01:54 +0100

    atomic register done, not tested

--- solution/src/atomic_register.rs
+++ solution/src/atomic_register.rs
@@ -1,4 +1,5 @@
-use std::collections::{BTreeMap, BTreeSet, HashSet};
+use core::time;
+use std::collections::{BTreeMap, BTreeSet, HashMap, HashSet};
 use std::future::Future;
 use std::pin::Pin;
 use std::sync::Arc;
@@ -19,11 +20,13 @@ pub struct Register {
     self_ident: u8,
 
     op_id: Uuid,
+    client_op_id: u64,
     reading: bool,
     writing: bool,
+    write_phase: bool,
     write_val: SectorVec,
     read_val: SectorVec,
-    read_list: BTreeMap<(u64, u8), SectorVec>,
+    read_list: HashMap<u8, (u64, u8, SectorVec)>,
     ack_list: HashSet<u8>,
     success_callback:
         Box<dyn FnOnce(OperationSuccess) -> Pin<Box<dyn Future<Output = ()> + Send>> + Send + Sync>,
@@ -56,11 +59,13 @@ impl Register {
             self_ident,
 
             op_id: Uuid::new_v4(),
+            client_op_id: 0,
             reading: false,
             writing: false,
+            write_phase: false,
             write_val: SectorVec(vec![]),
             read_val: SectorVec(vec![]),
-            read_list: BTreeMap::new(),
+            read_list: HashMap::new(),
             ack_list: HashSet::new(),
             success_callback: Box::new(dummy_callback),
 
@@ -85,6 +90,170 @@ impl Register {
             .broadcast(crate::Broadcast { cmd: Arc::new(cmd) })
             .await
     }
+
+    async fn store(&mut self, sector: (SectorVec, u64, u8)) {
+        self.manager.write(self.sector_idx, &sector).await;
+
+        self.timestamp = sector.1;
+        self.write_rank = sector.2;
+        self.val = sector.0;
+    }
+
+    async fn read_proc(&mut self, header: SystemCommandHeader) {
+        self.send(
+            SystemRegisterCommand {
+                header: SystemCommandHeader {
+                    process_identifier: self.self_ident,
+                    msg_ident: header.msg_ident,
+                    sector_idx: self.sector_idx,
+                },
+                content: SystemRegisterCommandContent::Value {
+                    timestamp: self.timestamp,
+                    write_rank: self.write_rank,
+                    sector_data: self.val.clone(),
+                },
+            },
+            header.process_identifier,
+        )
+        .await
+    }
+
+    async fn value(&mut self, from: u8, timestamp: u64, write_rank: u8, data: SectorVec) {
+        if self.write_phase {
+            log::debug!(
+                "atomic register ({}, {}) received a value message when in write phase",
+                self.sector_idx,
+                self.self_ident
+            );
+            return;
+        }
+
+        self.read_list.insert(from, (timestamp, write_rank, data));
+
+        if self.read_list.len() > (self.processes_count / 2).into()
+            && (self.reading || self.writing)
+        {
+            let mut v = SectorVec(vec![]);
+            std::mem::swap(&mut v, &mut self.val);
+
+            self.read_list
+                .insert(self.self_ident, (self.timestamp, self.write_rank, v));
+
+            let (mut maxts, mut rr) = (0, 0);
+
+            // highest(*)
+            let mut m = HashMap::new();
+            std::mem::swap(&mut m, &mut self.read_list);
+            for (t, w, v) in m.into_values() {
+                if (t, w) > (maxts, rr) {
+                    maxts = t;
+                    rr = w;
+                    self.read_val = v;
+                }
+            }
+
+            self.ack_list = HashSet::new();
+            self.write_phase = true;
+
+            if self.reading {
+                self.broadcast(SystemRegisterCommand {
+                    header: SystemCommandHeader {
+                        process_identifier: self.self_ident,
+                        msg_ident: self.op_id,
+                        sector_idx: self.sector_idx,
+                    },
+                    content: SystemRegisterCommandContent::WriteProc {
+                        timestamp: maxts,
+                        write_rank: rr,
+                        data_to_write: self.read_val.clone(),
+                    },
+                })
+                .await;
+            } else {
+                let sector = (self.write_val.clone(), maxts + 1, self.self_ident);
+                self.store(sector).await;
+
+                let mut v = SectorVec(vec![]);
+                std::mem::swap(&mut v, &mut self.write_val);
+
+                self.broadcast(SystemRegisterCommand {
+                    header: SystemCommandHeader {
+                        process_identifier: self.self_ident,
+                        msg_ident: self.op_id,
+                        sector_idx: self.sector_idx,
+                    },
+                    content: SystemRegisterCommandContent::WriteProc {
+                        timestamp: self.timestamp,
+                        write_rank: self.write_rank,
+                        data_to_write: v,
+                    },
+                })
+                .await;
+            }
+        }
+    }
+
+    async fn write_proc(
+        &mut self,
+        header: SystemCommandHeader,
+        timestamp: u64,
+        write_rank: u8,
+        data: SectorVec,
+    ) {
+        if (timestamp, write_rank) > (self.timestamp, self.write_rank) {
+            self.store((data, timestamp, write_rank)).await;
+        }
+
+        self.send(
+            SystemRegisterCommand {
+                header: SystemCommandHeader {
+                    process_identifier: self.self_ident,
+                    msg_ident: header.msg_ident,
+                    sector_idx: self.sector_idx,
+                },
+                content: SystemRegisterCommandContent::Ack,
+            },
+            header.process_identifier,
+        )
+        .await
+    }
+
+    async fn ack(&mut self, from: u8) {
+        if !self.write_phase {
+            log::debug!(
+                "atomic register ({}, {}) received an ack message when not in write phase",
+                self.sector_idx,
+                self.self_ident
+            );
+            return;
+        }
+
+        self.ack_list.insert(from);
+
+        if self.ack_list.len() > (self.processes_count / 2).into() && (self.reading || self.writing)
+        {
+            self.ack_list = HashSet::new();
+            self.write_phase = false;
+
+            let callback = std::mem::replace(&mut self.success_callback, Box::new(dummy_callback));
+
+            if self.reading {
+                let mut v = SectorVec(vec![]);
+                std::mem::swap(&mut v, &mut self.read_val);
+                self.reading = false;
+                callback(OperationSuccess {
+                    request_identifier: self.client_op_id,
+                    op_return: OperationReturn::Read(ReadReturn { read_data: v }),
+                });
+            } else {
+                self.writing = false;
+                callback(OperationSuccess {
+                    request_identifier: self.client_op_id,
+                    op_return: OperationReturn::Write,
+                });
+            }
+        }
+    }
 }
 
 #[async_trait::async_trait]
@@ -96,29 +265,74 @@ impl AtomicRegister for Register {
             dyn FnOnce(OperationSuccess) -> Pin<Box<dyn Future<Output = ()> + Send>> + Send + Sync,
         >,
     ) {
+        if cmd.header.sector_idx != self.sector_idx {
+            log::warn!("atomic register for sector {}, write rank {} received client request for sector {}", self.sector_idx, self.write_rank, cmd.header.sector_idx);
+            return;
+        }
+
         self.op_id = Uuid::new_v4();
-        self.read_list = BTreeMap::new();
+        self.client_op_id = cmd.header.request_identifier;
+        self.read_list = HashMap::new();
         self.ack_list = HashSet::new();
         self.success_callback = success_callback;
 
-        let mut message = SystemRegisterCommand { 
-            header: SystemCommandHeader { process_identifier: self.self_ident, msg_ident: self.op_id, sector_idx: self.sector_idx }, 
-            content: SystemRegisterCommandContent::ReadProc, 
+        let mut message = SystemRegisterCommand {
+            header: SystemCommandHeader {
+                process_identifier: self.self_ident,
+                msg_ident: self.op_id,
+                sector_idx: self.sector_idx,
+            },
+            content: SystemRegisterCommandContent::ReadProc,
         };
 
         match cmd.content {
             ClientRegisterCommandContent::Read => {
                 self.reading = true;
-            },
+            }
             ClientRegisterCommandContent::Write { data } => {
                 self.writing = true;
-            },
+            }
         };
 
         self.broadcast(message).await;
     }
 
     async fn system_command(&mut self, cmd: SystemRegisterCommand) {
-        todo!("atomicregister::systemcommand")
+        if cmd.header.sector_idx != self.sector_idx {
+            log::warn!("atomic register for sector {}, write rank {} received server request for sector {}", self.sector_idx, self.write_rank, cmd.header.sector_idx);
+            return;
+        }
+
+        match cmd.content {
+            SystemRegisterCommandContent::ReadProc => self.read_proc(cmd.header).await,
+            SystemRegisterCommandContent::Value {
+                timestamp,
+                write_rank,
+                sector_data,
+            } => {
+                if self.op_id == cmd.header.msg_ident {
+                    self.value(
+                        cmd.header.process_identifier,
+                        timestamp,
+                        write_rank,
+                        sector_data,
+                    )
+                    .await;
+                }
+            }
+            SystemRegisterCommandContent::WriteProc {
+                timestamp,
+                write_rank,
+                data_to_write,
+            } => {
+                self.write_proc(cmd.header, timestamp, write_rank, data_to_write)
+                    .await;
+            }
+            SystemRegisterCommandContent::Ack => {
+                if self.op_id == cmd.header.msg_ident {
+                    self.ack(cmd.header.process_identifier).await;
+                }
+            }
+        }
     }
 }
